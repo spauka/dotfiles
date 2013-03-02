1841494f29cb50f5c71afb7e4f365dc15aafe9b2
Sebastian Pauka <spau4795@uni.sydney.edu.au>
2013-03-02 17:39:00 +1100

    Update bash_it to the latest version.

--- .bash_it/aliases/available/vim.aliases.bash
+++ .bash_it/aliases/available/vim.aliases.bash
@@ -1,3 +1,4 @@
-#!/bin/bash
+cite 'about-alias'
+about-alias 'vim abbreviations'
 
 alias v='mvim --remote-tab'
--- .bash_it/plugins/available/rbenv.plugin.bash
+++ .bash_it/plugins/available/rbenv.plugin.bash
@@ -1,8 +1,10 @@
-#!/bin/bash
-
 # Load rbebv, if you are using it
+
+cite about-plugin
+about-plugin 'load rbenv, if you are using it'
+
 export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
+[[ `which rbenv` ]] && eval "$(rbenv init -)"
 
 # Load the auto-completion script if rbenv was loaded.
-source ~/.rbenv/completions/rbenv.bash
+[[ -e ~/.rbenv/completions/rbenv.bash ]] && source ~/.rbenv/completions/rbenv.bash
