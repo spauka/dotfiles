aa4f265c448eddfb9de5c0f6598d8529b27a4087
Sebastian Pauka <spauka@gmail.com>
2025-02-21 17:45:30 +1100

    Retry on connection failure too

--- scripts/spotmon.py
+++ scripts/spotmon.py
@@ -51,8 +51,12 @@ HEADER = {"Metadata": "true"}
 # Current version of the API
 QUERY_PARAMS = {"api-version": "2020-07-01"}
 
-
-def retry(starting_retry_wait=5.0, multiplier=1.5, exception=Exception):
+def retry(
+    starting_retry_wait=1.0,
+    multiplier=1.5,
+    max_wait=300.0,
+    exception: tuple[type[Exception], ...] | type[Exception] = Exception,
+):
     """
     Decorator to add timeout with exponential backoff to function
     on exception
@@ -60,46 +64,48 @@ def retry(starting_retry_wait=5.0, multiplier=1.5, exception=Exception):
 
     def retry_func(f):
         def f_with_retry(
-            *args, retry_wait=starting_retry_wait, retry_count=0, **kwargs
+            *args, retry_wait=starting_retry_wait, retry_count=0, max_wait=max_wait, **kwargs
         ):
             try:
                 return f(*args, **kwargs)
-            except Exception as e:
+            except Exception as e:  # pylint: disable=broad-exception-caught
                 if isinstance(e, exception):
-                    wait_time = retry_wait + random.uniform(1, retry_wait)
-                    next_retry_wait = retry_wait * multiplier
-                    logger.warning(
+                    wait_time = (retry_wait / 2) + random.uniform(1, retry_wait)
+                    next_retry_wait = min(retry_wait * multiplier, max_wait)
+                    logger.error(
                         (
                             "Retrying function %s due to exception %s. "
-                            "Attempt %d. Waiting %.2f to try again."
+                            "Attempt %d. Waiting %.2f to try again.",
                         ),
                         f.__name__,
                         e.__class__.__name__,
                         retry_count,
                         wait_time,
+                        exc_info=e,
                     )
                     sleep(wait_time)
-                    return f_with_retry(
-                        *args,
-                        retry_wait=next_retry_wait,
-                        retry_count=retry_count + 1,
-                        **kwargs,
-                    )
-                raise e
+                else:
+                    raise e
+            return f_with_retry(
+                *args,
+                retry_wait=next_retry_wait,
+                retry_count=retry_count + 1,
+                **kwargs,
+            )
 
         return f_with_retry
 
     return retry_func
 
 
-@retry(exception=requests.exceptions.Timeout)
+@retry(exception=(requests.exceptions.ConnectionError, requests.exceptions.Timeout))
 def get_scheduled_events():
     resp = requests.get(METADATA_URL, headers=HEADER, params=QUERY_PARAMS, timeout=5.0)
     data = resp.json()
     return data
 
 
-@retry(exception=requests.exceptions.Timeout)
+@retry(exception=(requests.exceptions.ConnectionError, requests.exceptions.Timeout))
 def confirm_scheduled_event(event_id):
     # This payload confirms a single event with id event_id
     # You can confirm multiple events in a single request if needed
